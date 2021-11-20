12cc1695eeb8201b7a45f71f02db274856bbfdbd
Vahann <yan.vahan@gmail.com>
2021-11-20 23:13:44 +0300

    Initial commit

---
+++ src/annotation/Validator.java
@@ -0,0 +1,28 @@
+package annotation;
+
+import annotation.myannotation.AdulthoodAnnotationProcessor;
+import annotation.myannotation.EmailAnnotationProcessor;
+import annotation.myannotation.IntervalAnnotationProcessor;
+import annotation.myannotation.LengthAnnotationProcessor;
+
+public class Validator {
+    /**
+     * Создайте класс Validator с одним методом проверки,
+     * который принимает в качестве входных данных dto
+     * и возвращает список ошибок, обнаруженных в процессе проверки.
+     * Класс должен работать с любым типом DTO
+     */
+    /**
+     * При чтении аннотаций из полей dto необходимо проверять каждое
+     * поле в зависимости от размещенной над ним аннотации.
+     * В случае ошибки добавьте его в итоговый список
+     */
+
+
+    public static void validate(Object ob) throws NoSuchFieldException, IllegalAccessException {
+        LengthAnnotationProcessor.definitionFieldsForAnnotationLength(ob);
+        EmailAnnotationProcessor.definitionFieldsForAnnotationEmail(ob);
+        IntervalAnnotationProcessor.definitionFieldsForAnnotationInterval(ob);
+        AdulthoodAnnotationProcessor.definitionFieldsForAnnotationLength(ob);
+    }
+}
